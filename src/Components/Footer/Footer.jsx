10e24736525e53da76184fc0d0b6df53c6106568
MNoumanAli <mnoumanali001@gmail.com>
2022-09-29 13:06:19 -0700

    Add Footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+        <div>
+            <div>
+                <div>
+                    <p>The</p>
+                    <p>Movie</p>
+                    <p>DB</p>
+                </div>
+
+                <div>
+                    <p>Join The Community</p>
+                </div>
+            </div>
+
+            <div>
+                <p className='footer-heading'>The Basics</p>
+                <p>About TMDB</p>
+                <p>Contact Us</p>
+                <p>Suppoprt For API</p>
+                <p>System Status</p>
+            </div>
+
+            <div>
+                <p className='footer-heading'>Get Invloved</p>
+                <p>Contribution</p>
+                <p>Add New Movie</p>
+                <p>Add New TV Show</p>
+            </div>
+            <div>
+                <p className='footer-heading'>Community</p>
+                <p>Guidlines</p>
+                <p>Discussion</p>
+                <p>LeaderBoard</p>
+                <p>Twitter</p>
+            </div>
+            <div>
+                <p className='footer-heading'>Legal</p>
+                <p>Term of Use</p>
+                <p>API Term of Use</p>
+                <p>Provacy Policy</p>
+            </div>
+        </div>     
+    </div>
+  )
+}
